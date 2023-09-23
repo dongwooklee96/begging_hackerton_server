0bc921fcd26c2bf9635e6a3a41b012f6f0d6a25c
DongWook Lee <sh95119@gmail.com>
2023-09-24 02:30:23 +0900

    FEAT: 게임방에 참여할 때 최대 수가 차면 참여할 수 없도록 로직 추가

--- src/router/game.py
+++ src/router/game.py
@@ -7,7 +7,7 @@ from sqlmodel import select
 from starlette.responses import JSONResponse
 
 from database.connection import get_session
-from database.models import Game, User, UserGame
+from database.models import Game, User, UserGame, Product
 from src.auth.auth import get_current_active_user
 
 game_router = APIRouter(
@@ -40,6 +40,20 @@ async def join_game(
     if len(user_game) > 0:
         return JSONResponse(
             content={"message": "해당 유저는 이미 게임에 참여하고 있습니다."}, status_code=400
+        )
+
+    # 게임에 참여한 유저가 최대 숫자라면 참여할 수 없다.
+    statement = select(UserGame).where(UserGame.game_key == game.game_key)
+    result = await session.execute(statement)
+    user_game = result.all()
+
+    statement = select(Product).where(Product.product_key == product_key)
+    result = await session.execute(statement)
+    product = result.scalar_one_or_none()
+
+    if len(user_game) >= product.max_participants:
+        return JSONResponse(
+            content={"message": "해당 게임은 이미 최대 인원이 참여하고 있습니다."}, status_code=400
         )
 
     user_game = UserGame(user_key=current_user.user_key, game_key=game.game_key)
