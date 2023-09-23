d600d125d6fe95b1e6bc3b28b876b58fdb511c75
DongWook Lee <sh95119@gmail.com>
2023-09-23 18:06:38 +0900

    FEAT: 테이블이 모델에 지정한대로 자동으로 생성되도록 변경

--- database/models.py
+++ database/models.py
@@ -4,12 +4,153 @@ from sqlmodel import Field
 from sqlmodel import SQLModel
 
 
-class User(SQLModel, table=True):
+# 공통 컬럼 모델
+# ----------------------------------------------------
+class TimestampModel(SQLModel):
+    created_at: datetime = Field(
+        default_factory=datetime.utcnow,
+        nullable=False,
+    )
+
+    updated_at: datetime = Field(
+        default_factory=datetime.utcnow,
+        nullable=False,
+    )
+
+
+class DeletedModel(SQLModel):
+    deleted: bool = Field(default_factory=False, nullable=False)
+
+
+# ----------------------------------------------------
+
+
+# 엔티티 테이블
+# ----------------------------------------------------
+class User(TimestampModel, DeletedModel, table=True):
+    """
+    유저 테이블
+    """
+
     __tablename__ = "user"
 
     user_key: int = Field(primary_key=True)
     email: str = Field()
     nickname: str = Field()
-    deleted: bool = Field()
-    created_at: datetime = Field()
-    updated_at: datetime = Field()
+
+
+class Category(TimestampModel, DeletedModel, table=True):
+    """
+    카테고리 테이블
+    """
+
+    __tablename__ = "category"
+
+    category: int = Field(primary_key=True)
+    category_name: str = Field()
+
+
+class Product(TimestampModel, DeletedModel, table=True):
+    """
+    상품 테이블
+    """
+
+    __tablename__ = "product"
+
+    product_key: int = Field(primary_key=True)
+    category_key: int = Field()
+    user_key: int = Field()
+
+
+class Prize(TimestampModel, DeletedModel, table=True):
+    """
+    업적 테이블
+    """
+
+    __tablename__ = "prize"
+
+    prize_key: int = Field(primary_key=True)
+    prize_name: str = Field()
+
+
+class Game(TimestampModel, DeletedModel, table=True):
+    """
+    게임 테이블
+    """
+
+    __tablename__ = "game"
+
+    game_key: int = Field(primary_key=True)
+    user_key: int = Field()
+
+
+class ClickGame(TimestampModel, table=True):
+    """
+    게임 (클릭) 테이블
+    """
+
+    __tablename__ = "click_game"
+    click_game_key: int = Field(primary_key=True)
+    game_key: int = Field()
+
+
+class TimeLimitGame(TimestampModel, table=True):
+    """
+    게임 (시간 제한) 테이블
+    """
+
+    __tablename__ = "time_limit_game"
+    time_limit_game_key: int = Field(primary_key=True)
+    game_key: int = Field()
+
+
+class ColorGame(TimestampModel, table=True):
+    """
+    게임 (틀린 색갈 찾기) 테이블
+    """
+
+    __tablename__ = "color_game"
+    color_game_key: int = Field(primary_key=True)
+    game_key: int = Field()
+
+
+# ----------------------------------------------------
+
+# 엔티티 관계 테이블
+# ----------------------------------------------------
+class PrizeUser(TimestampModel, table=True):
+    """
+    업적 - 유저 관계 테이블
+    """
+
+    __tablename__ = "prize_user"
+    prize_user_key: int = Field(primary_key=True)
+    prize_key: int = Field()
+    user_key: int = Field()
+
+
+class UserGame(TimestampModel, table=True):
+    """
+    유저 - 게임 테이블
+    게임에 참여하고 있는 유저의 관계를 보여준다
+    """
+
+    __tablename__ = "user_game"
+    user_game_key: int = Field(primary_key=True)
+    game_key: int = Field()
+    user_key: int = Field()
+
+
+class ProductGame(TimestampModel, table=True):
+    """
+    제품 - 게임 테이블
+    게임과 연관된 상품을 나타내는 테이블
+    """
+
+    __tablename__ = "product_game"
+    product_game_key: int = Field(primary_key=True)
+    product_key: int = Field()
+    game_key: int = Field()
+
+
+# ----------------------------------------------------
