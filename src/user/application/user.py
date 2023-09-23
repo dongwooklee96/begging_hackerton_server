902652fe839e58bfcb7198d5decb44273e18bb11
DongWook Lee <sh95119@gmail.com>
2023-09-24 05:04:16 +0900

    FEAT: 카카오 로그인 클라이언트 기준으로 변경

--- src/user/application/user.py
+++ src/user/application/user.py
@@ -1,4 +1,3 @@
-from database.models import User
 from src.user.repository.users import UserRepository
 
 
@@ -6,14 +5,22 @@ class UserService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
-    async def create_user(self, nickname: str, email: str):
+    async def create_user(self, id: int, nickname: str, profile_url: str):
         """
         유저를 생성한다.
         """
-        return await self.user_repository.create_user(nickname=nickname, email=email)
+        return await self.user_repository.create_user(
+            id=id, nickname=nickname, profile_url=profile_url
+        )
 
-    async def get_user_by_email(self, email: str) -> User:
+    async def get_user_by_id(self, id: int):
         """
-        유저를 유저 이메일로 조회한다.
+        유저를 유저 아이디로 조회한다.
         """
-        return await self.user_repository.get_user_by_email(email=email)
+        return await self.user_repository.get_user_by_id(id=id)
+
+    async def get_user_by_user_key(self, user_key: int):
+        """
+        유저를 유저 키로 조회한다.
+        """
+        return await self.user_repository.get_user_by_user_key(user_key=user_key)
