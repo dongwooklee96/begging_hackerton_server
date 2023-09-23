40e9b84a0e5e7e6ea5a4fd3a923af66bfd038d59
YoujeongChoi <70712293+YoujeongChoi@users.noreply.github.com>
2023-09-24 05:43:45 +0900

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+ALLOWED_HOSTS = [ '*' ] # 전체 설정
+ALLOWED_HOSTS = [ '113.125.131.81' ] # 예) 54.180.49.81
+
+CORS_ALLOW_ALL_ORIGINS = True # 전체 설정
+CORS_ALLOWED_ORIGINS = [
+		'http://113.125.131.81/', # 예) 'http://15.164.191.46/',
+		# '{도메인 주소}', # 예) 'https://movietrip.click/',
+]
